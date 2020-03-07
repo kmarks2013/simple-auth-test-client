8b1c1b9de17b37bb39cf1c9556c57eeca3d449b5
kmarks2013 <kmarks2013@gmail.com>
2020-03-06 19:38:58 -0500

    imports login page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import LoginPage from '../components/LoginPage'
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-            <h1>hi</h1>
+            <LoginPage />
             </div>
         )
     }
