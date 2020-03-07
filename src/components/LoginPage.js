bbf6d8c3d9b6b47841126335a33303578e558d96
kmarks2013 <kmarks2013@gmail.com>
2020-03-06 19:39:31 -0500

    adds  and console logs initial state:

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 
 export default class LoginPage extends Component {
+    state = {
+        usesrname: '',
+        password: ''
+    }
+
     render() {
+        console.log(this.state)
         return (
             <div>
                 I'm the Login Page
