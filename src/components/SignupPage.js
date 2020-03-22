b03ae19b96ffa58417694d3d3b2ad23b18eeb32b
kmarks2013 <kmarks2013@gmail.com>
2020-03-22 16:00:28 -0400

    adds Basic signup render with h1, form, and button to swtich to login

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 export default class SignupPage extends Component {
+
     render() {
         return (
             <div>
-                
+                <h1>Sign Up</h1>
+                <form>
+                    <label>Username</label>
+                    <input type="text" name="username"  ></input>
+                    <label>Password</label>
+                    <input type="password" name="password" ></input>
+                    <input type="submit" value="Sing Up"></input>
+                </form>
+                <label>Have an Account?</label>
+                <button onClick={() => this.props.redirect('login')}>Click to Login!</button>
             </div>
         )
     }
