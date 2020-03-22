4f703b2f10618aa69a8b922804b288779208d52d
kmarks2013 <kmarks2013@gmail.com>
2020-03-22 16:01:13 -0400

    adds state as values to useranme and password inputs

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -12,9 +12,9 @@ export default class SignupPage extends Component {
                 <h1>Sign Up</h1>
                 <form>
                     <label>Username</label>
-                    <input type="text" name="username"  ></input>
+                    <input type="text" name="username" value={this.state.username} ></input>
                     <label>Password</label>
-                    <input type="password" name="password" ></input>
+                    <input type="password" name="password" value={this.state.password}></input>
                     <input type="submit" value="Sing Up"></input>
                 </form>
                 <label>Have an Account?</label>
