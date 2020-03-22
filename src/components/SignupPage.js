ec7f2175e5b2de02e9cd8d7e5d10ea54100a882f
kmarks2013 <kmarks2013@gmail.com>
2020-03-22 16:00:52 -0400

    initalizes state for signup

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
 export default class SignupPage extends Component {
+    state = {
+        username: "",
+        password: ""
+    }
 
     render() {
         return (
